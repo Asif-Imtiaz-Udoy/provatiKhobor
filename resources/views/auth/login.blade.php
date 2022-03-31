552f4aef1a133f11dda5a9db34d6bf8fb74e32e7
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-03-31 14:11:13 +0600

    login blade modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,77 +18,80 @@
 </head>
 
 <body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="{{ route('home') }}" class="h1"><b>দৈনিক </b> প্রভাতী বাংলাদেশ</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 m-auto">
+                <div class="login-box">
+                    <!-- /.login-logo -->
+                    <div class="card card-outline card-primary">
+                        <div class="card-header text-center">
+                            <a href="{{ route('home') }}" class="h1"><b>দৈনিক </b> প্রভাতী খবর</a>
+                        </div>
+                        <div class="card-body">
+                            @if (session()->has('error'))
+                                <div class="alert alert-danger">
+                                    <p class="mb-0 text-center">{{ session()->get('error') }}</p>
+                                </div>
+                            @else
+                            <p class="login-box-msg">{{ session()->has('error') ? session()->get('error'):'Log In to start your session' }}</p>
+                            @endif
+            
+                            <form action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password" required
+                                        autocomplete="current-password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="icheck-primary">
+                                            <input type="checkbox" id="remember">
+                                            <label for="remember">
+                                                Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-4">
+                                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.login-box -->
             </div>
-            <div class="card-body">
-                @if (session()->has('error'))
-                    <div class="alert alert-danger">
-                        <p class="mb-0 text-center">{{ session()->get('error') }}</p>
-                    </div>
-                @else
-                <p class="login-box-msg">{{ session()->has('error') ? session()->get('error'):'Log In to start your session' }}</p>
-                @endif
-
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input id="password" type="password"
-                            class="form-control @error('password') is-invalid @enderror" name="password" required
-                            autocomplete="current-password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-                {{-- <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p> --}}
-            </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.login-box -->
 </body>
 
 </html>
